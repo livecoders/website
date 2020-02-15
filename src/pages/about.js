93436ea7909bcd58bbf30ecc0a811ef1dcbe5cca
Benjamin Lannon <benjamin@lannonbr.com>
2020-02-15 14:03:55 -0500

    Flesh out style for about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,31 +3,47 @@ import React from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
 
+import LiveCodersImg from "../img/livecoders-2.jpg";
+import ChatBubbleIcon from "../img/icons/comments-regular.svg";
+import HeartIcon from "../img/icons/heart-regular.svg";
+import KeyboardIcon from "../img/icons/keyboard-regular.svg";
+import UserIcon from "../img/icons/user-regular.svg";
+import TwitchIcon from "../img/icons/twitch-brands.svg";
+
+import "../css/about.css";
+
 export default () => (
   <div>
-    <Header />
-    <h1>About the Live Coders</h1>
-    <p>
-      We're a stream team of Live Coders who broadcast development-based
-      edutainment on Twitch
-    </p>
-    <section>
-      <h2>Who Are We?</h2>
-      <p>
-        On the Live Video platform, Twitch, we are a group of broadcasters who
-        write code and make things while teaching viewers. We believe that live
-        video is an amazing opportunity to teach, and want to share our
-        expertise.
-      </p>
-      <p>
-        You can find the team's page on Twitch at:
-        https://twitch.tv/team/livecoders
-      </p>
-      <a href="https://twitch.tv/team/livecoders" className="btn">
-        View Twitch Team Home Hub
-      </a>
+    <div className="head">
+      <Header />
+      <div>
+        <h1>About the Live Coders</h1>
+        <p>
+          We're a stream team of Live Coders who broadcast development-based
+          edutainment on Twitch
+        </p>
+      </div>
+    </div>
+    <section id="whoAreWe">
+      <div>
+        <h2>Who Are We?</h2>
+        <p>
+          On the Live Video platform, Twitch, we are a group of broadcasters who
+          write code and make things while teaching viewers. We believe that
+          live video is an amazing opportunity to teach, and want to share our
+          expertise.
+        </p>
+        <p>
+          You can find the team's page on Twitch at:
+          https://twitch.tv/team/livecoders
+        </p>
+        <a href="https://twitch.tv/team/livecoders" className="btn">
+          View Twitch Team Home Hub
+        </a>
+      </div>
+      <img src={LiveCodersImg} alt="Picture of some of the Live Coders" />
     </section>
-    <section>
+    <section id="qualifications">
       <h2>Qualifications to be a member</h2>
       <p>
         We want to ensure that broadcasters on the team are makers who have made
@@ -36,7 +52,9 @@ export default () => (
       </p>
       <div className="twoByThreeGrid">
         <div className="card">
-          <span className="icon"></span>
+          <span className="icon">
+            <img src={TwitchIcon} alt="Twitch Logo" />
+          </span>
           <h3>Be a Twitch Affiliate or Partner</h3>
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
@@ -46,7 +64,9 @@ export default () => (
           </p>
         </div>
         <div className="card">
-          <span className="icon"></span>
+          <span className="icon">
+            <img src={ChatBubbleIcon} alt="Chat Bubble" />
+          </span>
           <h3>During broadcasts, politely answer questions from viewers</h3>
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
@@ -56,7 +76,9 @@ export default () => (
           </p>
         </div>
         <div className="card">
-          <span className="icon"></span>
+          <span className="icon">
+            <img src={HeartIcon} alt="Heart" />
+          </span>
           <h3>Promote a positive, inclusive, and welcoming chat room</h3>
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
@@ -66,7 +88,9 @@ export default () => (
           </p>
         </div>
         <div className="card">
-          <span className="icon"></span>
+          <span className="icon">
+            <img src={UserIcon} alt="Person" />
+          </span>
           <h3>Are a person, not a brand or company account</h3>
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
@@ -76,7 +100,9 @@ export default () => (
           </p>
         </div>
         <div className="card">
-          <span className="icon"></span>
+          <span className="icon">
+            <img src={KeyboardIcon} alt="Keyboard" />
+          </span>
           <h3>Regularly write code or make things with technology</h3>
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
@@ -86,9 +112,18 @@ export default () => (
           </p>
         </div>
       </div>
-      <a href="#" className="btn">
-        Apply Today
-      </a>
+      <p>
+        Live coder team members will review and ensure that prospective channels
+        meet these requirements before admitting new memebers to the team
+      </p>
+      <div className="flex">
+        <a
+          href="https://github.com/livecoders/Home/issues/new?assignees=csharpfritz&labels=application&template=application.md&title=Application+for+Membership"
+          className="btn"
+        >
+          Apply Today
+        </a>
+      </div>
     </section>
     <Footer />
   </div>
