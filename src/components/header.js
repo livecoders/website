9c30006bc2711847fefe93beca166ae4534d40b2
Benjamin Lannon <benjamin@lannonbr.com>
2020-03-22 21:14:56 -0400

    Add top banner for conference

--- src/components/header.js
+++ src/components/header.js
@@ -8,11 +8,27 @@ import "../css/header.css";
 
 export default () => {
   return (
-    <header>
-      <Link to="/">
-        <img src={LogoImg} alt={"Live Coders Logo"} />
-      </Link>
-      <Nav />
-    </header>
+    <React.Fragment>
+      <div
+        style={{
+          backgroundColor: "#2c1643",
+          borderBottom: "3px solid rgb(32, 3, 62)",
+          padding: "10px 0px",
+          textAlign: "center",
+          marginBottom: 0
+        }}
+      >
+        Tune in to The Live Coders Conference. Coming April 9th.{" "}
+        <a style={{ color: "white " }} href="https://conf.livecoders.dev/">
+          Learn More
+        </a>
+      </div>
+      <header>
+        <Link to="/">
+          <img src={LogoImg} alt={"Live Coders Logo"} />
+        </Link>
+        <Nav />
+      </header>
+    </React.Fragment>
   );
 };
