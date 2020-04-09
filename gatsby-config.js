04ff4a3524bea733f546cffe1d0c7a4c1202de94
Benjamin Lannon <benjamin@lannonbr.com>
2020-04-09 16:43:45 -0400

    Adding google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,17 +4,24 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
-        path: `${__dirname}/src/`
-      }
+        path: `${__dirname}/src/`,
+      },
     },
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
-        extensions: [".md", ".mdx"]
-      }
+        extensions: [".md", ".mdx"],
+      },
     },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-react-helmet`
-  ]
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `G-NEXN42Y4HK`,
+        anonymize: true,
+      },
+    },
+  ],
 };
