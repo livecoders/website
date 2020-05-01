67ed90e049608cdbcc950ae1ca01137e0a0df7e6
Benjamin Lannon <benjamin@lannonbr.com>
2020-04-30 21:36:19 -0400

    Updating genmembers script to include access token secret

--- scripts/genMembers/index.js
+++ scripts/genMembers/index.js
@@ -1,58 +1,60 @@
-require("dotenv").config();
-const fetch = require("node-fetch");
-const fs = require("fs");
-const path = require("path");
+require("dotenv").config()
+const fetch = require("node-fetch")
+const fs = require("fs")
+const path = require("path")
 
-const clientId = process.env.TWITCH_CLIENT_ID;
-const team = "livecoders";
+const clientId = process.env.TWITCH_CLIENT_ID
+const clientId = process.env.TWITCH_CLIENT_SECRET
+const team = "livecoders"
 
-let teamURL = `https://api.twitch.tv/kraken/teams/${team}`;
+let teamURL = `https://api.twitch.tv/kraken/teams/${team}`
 
 // Pull team info from Twitch API
 fetch(teamURL, {
   headers: {
     Accept: "application/vnd.twitchtv.v5+json",
-    "Client-ID": clientId
-  }
+    "Client-ID": clientId,
+    Authorization: `OAuth ${clientSecret}`,
+  },
 })
-  .then(resp => resp.json())
-  .then(data => {
+  .then((resp) => resp.json())
+  .then((data) => {
     // grab just username and logo for each member
-    let users = data.users.map(user => {
-      return { name: user.name, logo: user.logo };
-    });
+    let users = data.users.map((user) => {
+      return { name: user.name, logo: user.logo }
+    })
 
     // grab all of the files currently in src/members/ (existing member pages)
     const files = fs
       .readdirSync(path.join(__dirname, "..", "..", "src", "members"))
-      .map(fileName => fileName.slice(0, -3));
+      .map((fileName) => fileName.slice(0, -3))
 
     // filter out users that already have pages
-    users = users.filter(user => !files.includes(user.name));
+    users = users.filter((user) => !files.includes(user.name))
 
     if (users.length === 0) {
-      console.log("No new members");
-      return;
+      console.log("No new members")
+      return
     }
 
     // write new pages for new members
-    users.forEach(user => {
+    users.forEach((user) => {
       const markdownContent = `---\nusername: ${
         user.name
       }\nprofile: ../img/members/${user.name}.${user.logo
         .split(".")
         .pop()}\n---\n
-      `;
+      `
 
       fs.writeFileSync(
         path.join(__dirname, "..", "..", "src", "members", `${user.name}.md`),
         markdownContent
-      );
+      )
 
       // Save images
       fetch(user.logo)
-        .then(resp => resp.arrayBuffer())
-        .then(buff => {
+        .then((resp) => resp.arrayBuffer())
+        .then((buff) => {
           fs.writeFileSync(
             path.join(
               __dirname,
@@ -64,7 +66,7 @@ fetch(teamURL, {
               `${user.name}.${user.logo.split(".").pop()}`
             ),
             Buffer.from(buff)
-          );
-        });
-    });
-  });
+          )
+        })
+    })
+  })
