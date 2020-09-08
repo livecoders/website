9b1eb60f04479f59a6ad22996c5e6ec46afa07b7
David Poindexter <drpoindexter@gmail.com>
2020-09-08 19:43:36 -0400

    Add initial template page for the next conference landing page.

--- engineer-notes.md
+++ engineer-notes.md
@@ -9,8 +9,13 @@
 
 You can also open a collab session with someone who has the fork already installed, and access their local server apart from viewing their terminal and code.
 
-# To-DO
+# To-Do
 
 - [x] Create new page not included in the folder "[pages](./src/pages)".
+- [ ] Make a style guide.
 - [ ] Create branded header component.
-  - If header component is disactivated or void, then ignore.
+  - If header component is **disactivated** or void, then ignore.
+- [ ] Add sections for LCC3.
+  - Make components for ease of use.
+- [ ] When ready, make new LCC page present on [nav](./src/components/nav.js).
+- [ ] Also when ready, use new branded header component for a new LCC3 banner.
--- src/pages/lcc3.js
+++ src/pages/lcc3.js
@@ -3,6 +3,22 @@ import React from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
 import SEO from "../components/seo"
+import Sponsor from "../components/sponsor"
+
+const SampleSponsors = [
+  {
+    image:
+      "https://media.discordapp.net/attachments/723153713480335440/747623069056761867/vuevember5.png?width=810&height=263",
+    alt: "NOT sveltember",
+    href: "https://w3schools.com",
+  },
+  {
+    image:
+      "https://media.discordapp.net/attachments/723153713480335440/747621734257131620/Sveltember8.png?width=810&height=251",
+    alt: "Sveltember",
+    href: "https://google.com",
+  },
+]
 
 export default () => (
   <React.Fragment>
@@ -11,9 +27,39 @@ export default () => (
       <Header />
       <div>
         <h1>Live Coders Conference 3, 2020</h1>
-        <p>Version COVID - What is time?</p>
+        <p>Wash hands, cover face, be nice</p>
       </div>
     </div>
+    <section id="timedate">Date Oct 29</section>
+    <section id="live">
+      <a
+        href="https://www.twitch.tv/livecoders"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Live Conference Stream
+      </a>
+    </section>
+    <section>
+      <title>
+        <h3>Agenda</h3>
+      </title>
+    </section>
+    <section id="sponsors">
+      <h1>Sponsors</h1>
+      {SampleSponsors && (
+        <React.Fragment>
+          {SampleSponsors.map((sponsor, key) => (
+            <Sponsor
+              image={sponsor.image}
+              alt={sponsor.alt}
+              href={sponsor.href}
+              key={key}
+            />
+          ))}
+        </React.Fragment>
+      )}
+    </section>
 
     <Footer />
   </React.Fragment>
