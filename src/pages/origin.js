3e0fba958b67469f5e44b02bc8fe228c78a1b6d2
Amelia Rose Aldridge <amelia@sanarinomi.com>
2020-10-09 00:00:02 +0200

    Origin placeholder

---
+++ src/pages/origin.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+import Header from "../components/header"
+import Footer from "../components/footer"
+import SEO from "../components/seo"
+
+import "../css/about.css"
+
+export default () => (
+  <React.Fragment>
+    <SEO title="Origin | The Live Coders" />
+    <div className="head">
+      <Header />
+      <div>
+        <h1>Origins of the Live Coders</h1>
+        <p>
+
+        </p>
+      </div>
+    </div>
+    <section id="originStart">
+      <div>
+        
+      </div>
+    </section>
+    <Footer />
+  </React.Fragment>
+)
