8d45ebe3af5620bcaeb0e7a394ffea8e9723d581
David Poindexter <drpoindexter@gmail.com>
2021-01-14 17:11:19 -0500

    Remove, make conditional conf banner

--- src/components/header.js
+++ src/components/header.js
@@ -4,11 +4,14 @@ import Nav from "../components/nav"
 
 import LogoImg from "../img/live-coder-transparent.png"
 
+import EventBanner from "./event-banner"
+
 import "../css/header.css"
 
 export default () => {
   return (
     <React.Fragment>
+      <EventBanner />
       <header>
         <Link to="/">
           <img src={LogoImg} alt={"Live Coders Logo"} />
