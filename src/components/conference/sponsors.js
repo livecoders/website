641ff6077289377efc71ea452d81294978739a8b
David Poindexter <drpoindexter@gmail.com>
2021-01-14 13:18:58 -0500

    Remove unused twitch-channel component, and unused vars

--- src/components/conference/sponsors.js
+++ src/components/conference/sponsors.js
@@ -2,19 +2,21 @@ import React from "react"
 import styled from "styled-components"
 let data = require("./sponsors.json")
 
-const teamSponsors = data.filter((item) => item.level === "Team");
-const sponsors = data.filter((item) => item.level !== "Team").sort((a,b) => {
-  let la = a.level.toLowerCase(),
-  lb = b.level.toLowerCase();
+const teamSponsors = data.filter((item) => item.level === "Team")
+const sponsors = data
+  .filter((item) => item.level !== "Team")
+  .sort((a, b) => {
+    let la = a.level.toLowerCase(),
+      lb = b.level.toLowerCase()
 
-  if (la < lb) {
-    return -1;
-  }
-  if (la > lb) {
-    return 1;
-  }
-  return 0;
-})
+    if (la < lb) {
+      return -1
+    }
+    if (la > lb) {
+      return 1
+    }
+    return 0
+  })
 
 const SponsorGrid = styled.section`
   display: grid;
@@ -29,42 +31,41 @@ const SponsorGrid = styled.section`
   }
 `
 
-
 const SponsorWrapper = styled.article`
-  margin: 5vh 15vw; 
+  margin: 5vh 15vw;
 
   @media screen and (max-width: 600px) {
-    margin: 5vh 5vw; 
+    margin: 5vh 5vw;
   }
 
   a {
     display: block;
     padding: 1vh 0;
   }
-
-`;
-
+`
 
 const Sponsor = ({ item }) => (
   <SponsorWrapper>
-    <a href={item.url} target="_blank"><img src={item.logo} alt={item.alt} /></a>
+    <a href={item.url} target="_blank" rel="noreferrer">
+      <img src={item.logo} alt={item.alt} />
+    </a>
   </SponsorWrapper>
-  )
+)
 
 export default () => (
   <>
     <br />
     <h3>Team Sponsors</h3>
     <SponsorGrid>
-    {teamSponsors.map(teamSponsor => (
-      <Sponsor item={teamSponsor} />
-    ))}
+      {teamSponsors.map((teamSponsor) => (
+        <Sponsor item={teamSponsor} />
+      ))}
     </SponsorGrid>
     <h3>Conference Sponsors</h3>
     <SponsorGrid>
-    {sponsors.map(sponsor => (
-      <Sponsor item={sponsor} />
-    ))}
+      {sponsors.map((sponsor) => (
+        <Sponsor item={sponsor} />
+      ))}
     </SponsorGrid>
   </>
 )
