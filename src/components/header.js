ab99f714ba424a32e20ad2e98be84d3eab82408b
David Poindexter <drpoindexter@gmail.com>
2021-04-08 13:47:56 -0400

    Closes 211, removees stale conf banner

--- src/components/header.js
+++ src/components/header.js
@@ -4,18 +4,11 @@ import Nav from "../components/nav"
 
 import LogoImg from "../img/live-coder-transparent.png"
 
-import EventBanner from "./event-banner"
-
 import "../css/header.css"
 
 export default () => {
   return (
     <React.Fragment>
-      <EventBanner
-        link={"conference"}
-        date={"March 8th, 2021"}
-        title={"International Womens' Day Event"}
-      />
       <header>
         <Link to="/">
           <img src={LogoImg} alt={"Live Coders Logo"} />
